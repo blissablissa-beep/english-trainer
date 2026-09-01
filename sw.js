9758b756baa768311513b8d311f0abec984732a7
blissablissa-beep <blissablissa@gmail.com>
2026-09-01 13:59:28 +0900

    sw.js aktualisieren

--- sw.js
+++ sw.js
@@ -1,56 +1,86 @@
 "use strict";
 
-const CACHE_NAME = "english-trainer-v1";
+const CACHE_NAME =
+  "english-trainer-v4-split-files";
 
 const APP_FILES = [
   "./",
   "./index.html",
+  "./styles.css",
+  "./app.js",
   "./cards.json",
   "./manifest.webmanifest"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches
-      .open(CACHE_NAME)
-      .then(cache => cache.addAll(APP_FILES))
-  );
+self.addEventListener(
+  "install",
+  event => {
+    event.waitUntil(
+      caches
+        .open(CACHE_NAME)
+        .then(cache =>
+          cache.addAll(APP_FILES)
+        )
+    );
 
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      )
-    )
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  if (event.request.method !== "GET") {
-    return;
+    self.skipWaiting();
   }
+);
 
-  event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        const copy = response.clone();
+self.addEventListener(
+  "activate",
+  event => {
+    event.waitUntil(
+      caches
+        .keys()
+        .then(cacheNames =>
+          Promise.all(
+            cacheNames
+              .filter(
+                name =>
+                  name !== CACHE_NAME
+              )
+              .map(name =>
+                caches.delete(name)
+              )
+          )
+        )
+        .then(() =>
+          self.clients.claim()
+        )
+    );
+  }
+);
 
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
+self.addEventListener(
+  "fetch",
+  event => {
+    if (
+      event.request.method !== "GET"
+    ) {
+      return;
+    }
 
-        return response;
-      })
-      .catch(() =>
-        caches.match(event.request)
-      )
-  );
-});
+    event.respondWith(
+      fetch(event.request)
+        .then(response => {
+          const copy =
+            response.clone();
+
+          caches
+            .open(CACHE_NAME)
+            .then(cache => {
+              cache.put(
+                event.request,
+                copy
+              );
+            });
+
+          return response;
+        })
+        .catch(() =>
+          caches.match(event.request)
+        )
+    );
+  }
+);
